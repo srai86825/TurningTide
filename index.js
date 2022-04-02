c98174755a2033e633b0e63785db1d750751ab08
srai <91911821+srai86825@users.noreply.github.com>
2022-04-02 22:35:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function dropdown(){
+  // document.querySelector("select").toggleA
+  // alert("fuck");
+  document.querySelector(".pages-list").classList.toggle("hide");
+}
+function subscribed(){
+ // document.querySelector(".subscribe-text").style.backgroundColor="rgb(83 147 141);";
+ // document.querySelector(".subscribe-text").style.border="4px ridge #ffff";
+  document.querySelector(".subscribe-text").classList.toggle("subscribe-click");
+}
+function msgtn(){
+  document.querySelector(".contact-us button").classList.toggle("btn-msg-click");
+}
